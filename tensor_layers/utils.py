eeca11dd0a108720dbe27e03c6be023dcae9c821
Srinath Srinivasan <ssrini27@ncsu.edu>
2023-10-24 23:30:46 -0400

    trying back on host

--- tensor_layers/utils.py
+++ tensor_layers/utils.py
@@ -92,7 +92,7 @@ class TT_forward(torch.autograd.Function):
                 left.append(output)
             
             output = torch.from_numpy(cupy.asnumpy(output))
-            output.cuda()
+            output.cpu()
         
             output = torch.tensordot(matrix,output,[list(range(1,d+1)),list(range(d))])
 
