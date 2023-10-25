65d6fb7e6bf304b03355d959492d4b3e83150542
Srinath Srinivasan <ssrini27@ncsu.edu>
2023-10-24 23:27:07 -0400

    converting back to tensor

--- tensor_layers/utils.py
+++ tensor_layers/utils.py
@@ -91,6 +91,7 @@ class TT_forward(torch.autograd.Function):
                 print(output.shape)
                 left.append(output)
             
+            output = torch.from_numpy(output)
         
             output = torch.tensordot(matrix,output,[list(range(1,d+1)),list(range(d))])
 
