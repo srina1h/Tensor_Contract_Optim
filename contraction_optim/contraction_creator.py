ba5ce74efe5ebfbca81faf5b9429d741a9682ead
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-04-15 12:41:12 -0400

    cehcking if grad enabled

--- contraction_optim/contraction_creator.py
+++ contraction_optim/contraction_creator.py
@@ -43,6 +43,7 @@ class contraction_handler:
             # print(self.c.shape)
 
         # Perform the contraction
+        print(torch.is_grad_enabled())
         if torch.is_grad_enabled():
             output = cutensor.contraction(self.alpha_val, cp.from_dlpack((self.a.contiguous()).detach()), self.mode_a, cp.from_dlpack((self.b.contiguous()).detach()), self.mode_b, self.beta_val, self.c, self.mode_c, algo = self.contraction_algorithm)
             return torch.from_dlpack(output).requires_grad_(True)
