a4c3c020085079df3fd171362247b891fa4dfa89
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-04-15 12:34:30 -0400

    trying inf without detach

--- contraction_optim/contraction_creator.py
+++ contraction_optim/contraction_creator.py
@@ -43,8 +43,8 @@ class contraction_handler:
             # print(self.c.shape)
 
         # Perform the contraction
-        output = cutensor.contraction(self.alpha_val, cp.from_dlpack((self.a.contiguous()).detach()), self.mode_a, cp.from_dlpack((self.b.contiguous()).detach()), self.mode_b, self.beta_val, self.c, self.mode_c, algo = self.contraction_algorithm)
-        return torch.from_dlpack(output).requires_grad_()
+        output = cutensor.contraction(self.alpha_val, cp.from_dlpack((self.a.contiguous())), self.mode_a, cp.from_dlpack((self.b.contiguous())), self.mode_b, self.beta_val, self.c, self.mode_c, algo = self.contraction_algorithm)
+        return torch.from_dlpack(output)
     
     def construct_einstein_notation(self, aNoDim: int, bNoDim: int, contraction_indices: tuple[list, list]):
         indices = 'abcdefghijklmnopqrstuvwxyz'
