571487942ea8461f38dcd746c2230cd390e0c327
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-08-26 09:57:10 -0400

    profiling single contraction

--- tensor_layers/utils.py
+++ tensor_layers/utils.py
@@ -61,6 +61,7 @@ class TT_forward(torch.autograd.Function):
                 # output = (torch.tensordot(output, core, dims=([-1], [0])))
                 # print(output.requires_grad)
                 con = contraction_handler(output, core, ([-1], [0]))
+                exit()
                 output = (con.perform_contraction())
                 left.append(output)
             
