e3d02f088e4f414ab24e9f5c1f30b5412360cc14
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-09-16 10:36:55 -0400

    removign exit for profiling

--- tensor_layers/utils.py
+++ tensor_layers/utils.py
@@ -228,14 +228,14 @@ class TT_forward(torch.autograd.Function):
 
 
             # dx = (torch.tensordot(dx, temp.reshape(np.prod(tt_shape_row), -1), dims=([-1], [-1])))
-            print("Interested contraction")
-            print(dx.shape)
-            print(temp.reshape(np.prod(tt_shape_row), -1).shape)
+            # print("Interested contraction")
+            # print(dx.shape)
+            # print(temp.reshape(np.prod(tt_shape_row), -1).shape)
             con = contraction_handler(dx, temp.reshape(np.prod(tt_shape_row), -1), ([-1], [-1]))
             dx = (con.perform_contraction())
-            print("end of contraction")
-            if dx.shape == (4096, 20) and temp.reshape(np.prod(tt_shape_row), -1).shape == (768, 20):
-                exit()
+            # print("end of contraction")
+            # if dx.shape == (4096, 20) and temp.reshape(np.prod(tt_shape_row), -1).shape == (768, 20):
+            #     exit()
             dx = torch.reshape(dx,ctx.input_shape)            
 
             all_grads = [g for g in left_grads+right_grads]
