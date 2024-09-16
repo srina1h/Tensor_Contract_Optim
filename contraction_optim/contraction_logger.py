c13c5aacb2a358b4681c6a367835b85d475b32d1
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-09-16 11:14:12 -0400

    adding dimension logging

---
+++ contraction_optim/contraction_logger.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import pickle
+import os
+import sys
+
+def log_arguments(*args):
+    if len(args) != 7:
+        raise ValueError("Exactly 7 arguments are required")
+
+    log_file = 'logged_dimensions_tensorized_nn.pkl'
+    
+    # Load existing dataframe from pickle if it exists
+    if os.path.exists(log_file):
+        with open(log_file, 'rb') as f:
+            df = pickle.load(f)
+    else:
+        df = pd.DataFrame(columns=["original_A", "original_B", "A", "B", "C", "contraction_dim", "type", "number"])
+    
+    # Append new arguments to the dataframe
+    # Check if there are existing entries with the same values in A, B, and C columns
+    existing_entry = df[(df['A'] == args[2]) & (df['B'] == args[3]) & (df['C'] == args[4])]
+
+    if not existing_entry.empty:
+        # If such entries exist, increase their number in the 'number' column
+        df.loc[existing_entry.index, 'number'] += 1
+    else:
+        # If no such entries exist, add a new entry with 'number' set to 1
+        new_entry = pd.DataFrame([args + (1,)], columns=df.columns.tolist() + ['number'])
+        df = pd.concat([df, new_entry], ignore_index=True)
+    
+    # Save the updated dataframe back to pickle
+    with open(log_file, 'wb') as f:
+        pickle.dump(df, f)
+
+if __name__ == "__main__":
+    log_arguments(*sys.argv[1:])
--- tensor_layers/utils.py
+++ tensor_layers/utils.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import cupy as cp
 # from cupyx import cutensor
 from contraction_optim.contraction_creator import contraction_handler
+from contraction_optim import contraction_logger
 
 class config_class():
     def __init__(self,
@@ -61,14 +62,18 @@ class TT_forward(torch.autograd.Function):
                 # output = (torch.tensordot(output, core, dims=([-1], [0])))
                 # print(output.requires_grad)
                 con = contraction_handler(output, core, ([-1], [0]))
+                temp_a = output
                 output = (con.perform_contraction())
+                contraction_logger.log_arguments(None, None, temp_a.shape, core.shape, output.shape, ([-1], [0]), "forward")
                 # exit()
                 left.append(output)
             
         
             # output = torch.tensordot(matrix,output,[list(range(1,d+1)),list(range(d))])
+            temp_b = output
             con = contraction_handler(matrix, output, [list(range(1,d+1)),list(range(d))])
             output = con.perform_contraction()
+            contraction_logger.log_arguments(None, None, matrix.shape, temp_b.shape, output.shape, [list(range(1,d+1)),list(range(d))], "forward")
 
 
             saved_tensors.append(left)
@@ -77,8 +82,10 @@ class TT_forward(torch.autograd.Function):
             right.append(temp)
             for core in factors[d + 1:]:
                 # temp = (torch.tensordot(temp, core, dims=([-1], [0])))
+                temp_a = temp
                 con = contraction_handler(temp, core, ([-1], [0]))
                 temp = (con.perform_contraction())
+                contraction_logger.log_arguments(None, None, temp_a.shape, core.shape, temp.shape, ([-1], [0]), "forward")
                 right.append(temp)
 
             out = torch.squeeze(temp)
@@ -86,8 +93,10 @@ class TT_forward(torch.autograd.Function):
             
             
             # output = torch.tensordot(output,out,[[-1],[0]])
+            temp_a = output
             con = contraction_handler(output,out,([-1],[0]))
             output = (con.perform_contraction())
+            contraction_logger.log_arguments(None, None, temp_a.shape, out.shape, output.shape, ([-1],[0]), "forward")
             output = torch.reshape(output,out_shape)
         
             
@@ -127,8 +136,11 @@ class TT_forward(torch.autograd.Function):
 
         
             # dy_core_prod = (torch.tensordot(dy, dy_core_prod.reshape(dy_core_prod.shape[0], -1), dims=([1], [1])))
+            temp_b = dy_core_prod
+            actual_b = dy_core_prod.reshape(dy_core_prod.shape[0], -1)
             con = contraction_handler(dy, dy_core_prod.reshape(dy_core_prod.shape[0], -1), ([1], [1]))
             dy_core_prod = (con.perform_contraction())
+            contraction_logger.log_arguments(None, temp_b.shape, dy.shape, actual_b.shape, dy_core_prod.shape, ([1], [1]), "backward")
 
             # matrix_dy_core_prod = torch.tensordot(matrix, dy_core_prod, dims=([0], [0]))
             con = contraction_handler(matrix, dy_core_prod, ([0], [0]))
@@ -137,28 +149,40 @@ class TT_forward(torch.autograd.Function):
 
             for i in reversed(range(1, d)):
                 # grad = (torch.tensordot(left[i - 1].reshape(-1, ranks[i]), matrix_dy_core_prod.reshape(np.prod(tt_shape_row[:i]), tt_shape_row[i], -1,ranks[d]),dims=([0], [0])))
+                temp_a = left[i-1]
+                temp_b = matrix_dy_core_prod
+                actual_a = left[i-1].reshape(-1, ranks[i])
+                actual_b = matrix_dy_core_prod.reshape(np.prod(tt_shape_row[:i]), tt_shape_row[i], -1,ranks[d])
                 con = contraction_handler(left[i - 1].reshape(-1, ranks[i]),
                                     matrix_dy_core_prod.reshape(np.prod(tt_shape_row[:i]), tt_shape_row[i], -1, ranks[d]), ([0], [0]))
                 grad = (con.perform_contraction())
+                contraction_logger.log_arguments(temp_a.shape, temp_b.shape, actual_a.shape, actual_b.shape, grad.shape, ([0], [0]), "backward")
                 # print(grad.shape)
                 if i == d - 1:
                     right_core = factors[i]
                 else:
                     # grad = (torch.tensordot(grad, right_core, dims=([2, 3], [1, 2])))
+                    temp_a = grad
                     con = contraction_handler(grad, right_core, ([2, 3], [1, 2]))
                     grad = (con.perform_contraction())
+                    contraction_logger.log_arguments(None, None, temp_a.grad, right_core.shape, grad.shape, ([2, 3], [1, 2]), "backward")
 
                     # right_core = torch.tensordot(factors[i], right_core,dims=([-1], [0])).reshape(ranks[i], -1, ranks[d])
+                    temp_b = right_core
                     con = contraction_handler(factors[i], right_core, ([-1], [0]))
                     right_core = con.perform_contraction().reshape(ranks[i], -1, ranks[d])
+                    contraction_logger.log_arguments(None, None, factors[i].shape, temp_b.shape, right_core.shape, ([-1], [0]), "backward")
+
                 
                 if grad.shape != factors[i].shape:
                     grad = grad.reshape(list(factors[i].shape))
                 # print(grad.shape)
                 left_grads.append(grad)
             # temp = (torch.tensordot(matrix_dy_core_prod.reshape(tt_shape_row[0], -1, ranks[d]), right_core, dims=([1, 2], [1, 2])).reshape(1, tt_shape_row[0], -1))
+            temp_a = matrix_dy_core_prod
             con = contraction_handler(matrix_dy_core_prod.reshape(tt_shape_row[0], -1, ranks[d]), right_core, ([1, 2], [1, 2]))
             temp = (con.perform_contraction().reshape(1, tt_shape_row[0], -1))
+            contraction_logger.log_arguments(temp_a.shape, None, matrix_dy_core_prod.reshape(tt_shape_row[0], -1, ranks[d]).shape, right_core.shape, temp.shape, ([1, 2], [1, 2]), "backward")
 
 
             left_grads.append(temp)
@@ -167,32 +191,42 @@ class TT_forward(torch.autograd.Function):
 
             matrix_core_prod = left[-1]
             # matrix_core_prod = (torch.tensordot(matrix_core_prod.reshape(-1, matrix_core_prod.shape[-1]), matrix, dims=([0], [1])))
+            temp_a = matrix_core_prod
+            actual_a = matrix_core_prod.reshape(-1, matrix_core_prod.shape[-1])
             con = contraction_handler(matrix_core_prod.reshape(-1, matrix_core_prod.shape[-1]), matrix, ([0], [1]))
             matrix_core_prod = (con.perform_contraction())
+            contraction_logger.log_arguments(temp_a.shape, None, actual_a.shape, matrix.shape, matrix_core_prod.shape, ([0], [1]), "backward")
             
             # print('dx=',torch.max(matrix_core_prod))
             # matrix_dy_core_prod = (torch.tensordot(matrix_core_prod, dy, dims=([1], [0])))
             con = contraction_handler(matrix_core_prod, dy, ([1], [0]))
             matrix_dy_core_prod = (con.perform_contraction())
+            contraction_logger.log_arguments(None, None, matrix_core_prod.shape, dy.shape, matrix_dy_core_prod.shape, ([1], [0]), "backward")
 
 
             for i in reversed(range(1, d)):
                 # grad = (torch.tensordot(right[i - 1].reshape(-1, ranks[d + i]),matrix_dy_core_prod.reshape(-1, tt_shape_col[i], int(np.prod(tt_shape_col[i + 1:]))),dims=([0], [0])))
+                
                 con = contraction_handler(right[i - 1].reshape(-1, ranks[d + i]),matrix_dy_core_prod.reshape(-1, tt_shape_col[i], int(np.prod(tt_shape_col[i + 1:]))), ([0], [0]))
                 grad = (con.perform_contraction())
+                contraction_logger.log_arguments(None, None, right[i - 1].reshape(-1, ranks[d + i]).shape, matrix_dy_core_prod.reshape(-1, tt_shape_col[i], int(np.prod(tt_shape_col[i + 1:]))).shape, grad.shape, ([0], [0]), "backward")
 
                 if i == d - 1:
                     right_core = factors[d + i].reshape(-1, tt_shape_col[i])
                 else:
                 
                     # grad = (torch.tensordot(grad, right_core, dims=([-1], [1])))
+                    actual_a = grad
                     con = contraction_handler(grad, right_core, ([-1], [1]))
                     grad = (con.perform_contraction())
+                    contraction_logger.log_arguments(None, None, actual_a.shape, right_core.shape, grad.shape, ([-1], [1]), "backward")
 
 
                     # right_core = (torch.tensordot(factors[d + i], right_core, dims=([-1], [0])).reshape(ranks[d + i],-1))
+                    actual_b = right_core
                     con = contraction_handler(factors[d + i], right_core, ([-1], [0]))
                     right_core = (con.perform_contraction().reshape(ranks[d + i],-1))
+                    contraction_logger.log_arguments(None, None, factors[d + i].shape, actual_b.shape, right_core.shape, ([-1], [0]), "backward")
                                                                                                                                                                             
                 if grad.shape != factors[d + i].shape:
                     grad = grad.reshape(list(factors[i].shape))
@@ -202,6 +236,7 @@ class TT_forward(torch.autograd.Function):
             # temp = (torch.tensordot(matrix_dy_core_prod.reshape(ranks[d], tt_shape_col[0], -1), right_core, dims=([-1], [1])))
             con = contraction_handler(matrix_dy_core_prod.reshape(ranks[d], tt_shape_col[0], -1), right_core, ([-1], [1]))
             temp = (con.perform_contraction())
+            contraction_logger.log_arguments(None, None, matrix_dy_core_prod.reshape(ranks[d], tt_shape_col[0], -1).shape, right_core.shape, temp.shape, ([-1], [1]), "backward")
 
             right_grads.append(temp)
 
@@ -210,29 +245,40 @@ class TT_forward(torch.autograd.Function):
             dx = factors[-1].reshape(ranks[-2], -1)
             for core in reversed(factors[d:-1]):
                 # dx = (torch.tensordot(core, dx, dims=([-1], [0])))
+                actual_b = dx
                 con = contraction_handler(core, dx, ([-1], [0]))
                 dx = (con.perform_contraction())
+                contraction_logger.log_arguments(None, None, core.shape, actual_b.shape, dx.shape, ([-1], [0]), "backward")
 
         
             # dx = (torch.tensordot(dy, dx.reshape(-1, np.prod(tt_shape_col)), dims=([-1], [-1])))
+            temp_b = dx
+            actual_b = dx.reshape(-1, np.prod(tt_shape_col))
             con = contraction_handler(dy, dx.reshape(-1, np.prod(tt_shape_col)), ([-1], [-1]))
             dx = (con.perform_contraction())
+            contraction_logger.log_arguments(None, temp_b.shape, dy.shape, actual_b.shape, dx.shape, ([-1], [-1]), "backward")
 
 
 
             temp = factors[0].reshape(-1, ranks[1])
             for core in factors[1:d]:
                 # temp = (torch.tensordot(temp, core, dims=([-1], [0])))
+                actual_a = temp
                 con = contraction_handler(temp, core, ([-1], [0]))
                 temp = (con.perform_contraction())
+                contraction_logger.log_arguments(None, None, actual_a.shape, core.shape, temp.shape, ([-1], [0]), "backward")
 
 
             # dx = (torch.tensordot(dx, temp.reshape(np.prod(tt_shape_row), -1), dims=([-1], [-1])))
             # print("Interested contraction")
             # print(dx.shape)
             # print(temp.reshape(np.prod(tt_shape_row), -1).shape)
+            actual_a = dx
+            temp_b = temp
+            actual_b = temp.reshape(np.prod(tt_shape_row), -1)
             con = contraction_handler(dx, temp.reshape(np.prod(tt_shape_row), -1), ([-1], [-1]))
             dx = (con.perform_contraction())
+            contraction_logger.log_arguments(None, temp_b, actual_a.shape, actual_b.shape, dx.shape, ([-1], [-1]), "backward")
             # print("end of contraction")
             # if dx.shape == (4096, 20) and temp.reshape(np.prod(tt_shape_row), -1).shape == (768, 20):
             #     exit()
