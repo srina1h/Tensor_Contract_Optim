2eb4cbe1abc419f44914a24162dd0c0f9264381c
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-09-16 11:51:34 -0400

    adding pickle extraction

---
+++ contraction_optim/extract_dimensions.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import pickle
+
+# Path to the pickle file
+pickle_file_path = '/home/ssrini27/Tensor_Contract_Optim/logged_dimensions_tensorized_nn.pk'
+
+# Load the pickle file into a DataFrame
+with open(pickle_file_path, 'rb') as file:
+    data = pickle.load(file)
+
+df = pd.DataFrame(data)
+
+# Export the DataFrame to an xlsx file
+xlsx_file_path = 'dimensions.xlsx'
+df.to_excel(xlsx_file_path, index=False)
+
+print(f"Data has been successfully exported to {xlsx_file_path}")
