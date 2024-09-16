31ecf9c44d54a254f416b1800dd063f3ca5376f0
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-09-16 11:53:19 -0400

    edit file name

--- contraction_optim/extract_dimensions.py
+++ contraction_optim/extract_dimensions.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pickle
 
 # Path to the pickle file
-pickle_file_path = '/home/ssrini27/Tensor_Contract_Optim/logged_dimensions_tensorized_nn.pk'
+pickle_file_path = '/home/ssrini27/Tensor_Contract_Optim/logged_dimensions_tensorized_nn.pkl'
 
 # Load the pickle file into a DataFrame
 with open(pickle_file_path, 'rb') as file:
